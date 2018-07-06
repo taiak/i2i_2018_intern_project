a9a9cf6c1ba4aae44670ca0461c219798426ca38
hamzaCakmak <40656986+hamzaCakmak@users.noreply.github.com>
2018-07-06 09:28:44 +0300

    project

--- oracle_database/cell2iTest.sql
+++ oracle_database/cell2iTest.sql
@@ -1,3 +1,26 @@
-BEGIN
-hr.cell2i_utility.check_user('905346319558');
-END;
+DECLARE
+    i                NUMBER;
+    v_returnref      SYS_REFCURSOR;
+    v_sub_id         NUMBER;
+    v_invoice_id     VARCHAR2(100);
+    v_invoice_date   DATE;
+    v_due_date       DATE;
+    v_total_amount   NUMBER;
+    v_open_amount    NUMBER; 
+
+BEGIN 
+     v_returnref := cell2i.cell2i_utility.get_invoiceinfo('12345'); 
+    LOOP FETCH v_returnref INTO
+    v_sub_id,
+    v_invoice_id,
+    v_invoice_date,
+    v_due_date,
+    v_total_amount,
+    v_open_amount; exit WHEN
+    v_returnref%notfound;
+    dbms_output.put_line(v_sub_id);
+    END
+loop;
+
+end;
+/
