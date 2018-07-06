d2a16b24b57be9e285d2cd84c31581742992d242
Zeynep Düzyurt <zeynepduzyurt34@gmail.com>
2018-07-06 09:35:26 +0300

    LoginAsyncTask

--- android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/SubScribe.java
+++ android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/SubScribe.java
@@ -5,19 +5,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.widget.EditText;
 
 /**
  * Created by Zeynep on 4.07.2018.
  */
 
-public class SubScribe extends Activity {
+public class SubScribe extends MainActivity  {
 
     public static boolean checkUsername(String username, String password) {
 
         boolean is_valid = false;
         if (!username.isEmpty() && !password.isEmpty()) {
-             if (username.equals("a") && password.equals("a"))
+
 
 
                 is_valid = true;
@@ -41,6 +42,11 @@ public class SubScribe extends Activity {
         return is_valid;
     }
 
+
+
+
+
+    //** login thread sonu
 
 
 }
