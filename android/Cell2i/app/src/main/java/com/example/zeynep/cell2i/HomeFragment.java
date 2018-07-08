9b6efd7451b517001e20a7df59a7204b3caf373c
rumeysadincsoy <rumeysadincsoy.rds@gmail.com>
2018-07-08 19:56:33 +0300

    update

---
+++ android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.example.zeynep.cell2i;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+
+    public void clickMethod() {
+
+        Log.d("home fragment" , "girildi");
+    }
+
+
+}
--- android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/HomePage.java
+++ android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/HomePage.java
@@ -1,20 +1,75 @@
 package com.example.zeynep.cell2i;
 
+import android.app.Fragment;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 
-public class HomePage extends FragmentActivity {
+public class HomePage extends Activity {
 
+    Button homepage, giftpage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        homepage=(Button)findViewById(R.id.homepage);
+        giftpage=(Button)findViewById(R.id.giftpage);
 
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        giftpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new GiftFragment());
+            }
+        });
+
+
+        }
+
+
+    private void loadFragment(Fragment fragment){
+            FragmentManager fm=getFragmentManager();
+            FragmentTransaction ft =fm.beginTransaction();
+            ft.replace(R.id.frameLayout,fragment);
+            ft.commit();
+        }
+
+
+
+
+
+    /*
+    public void FragmentHomeClick(View v){
+        new HomeFragment().clickMethod();
 
     }
+    public void FragmentGiftClick(View v){
+        new GiftFragment().clickMethod();
+    }
+
+
+    public void FragmentGiftClick(View view) {
+        Fragment myfragment;
+        myfragment = new Fragment();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }*/
+
+
 
 }
