e6be0e4f038cb5b254163d5a2bc098d777ef9fd4
rumeysadincsoy <rumeysadincsoy.rds@gmail.com>
2018-07-10 17:37:59 +0300

    update

--- android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/HomePage.java
+++ android/Cell2i/app/src/main/java/com/example/zeynep/cell2i/HomePage.java
@@ -5,20 +5,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
 public class HomePage extends Activity {
 
-    static TextView fullname;
-    static TextView no;
-    static TextView tariff;
-    Button exit;
-    Button invoices;
+    static TextView fullname,no,tariff;
+    static TextView sumint, summin, sumsms;
 
+    Button exit,invoices;
+    ProgressBar kalandk;
+    ProgressBar kalansms;
+    ProgressBar kalangb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +33,12 @@ public class HomePage extends Activity {
         fullname.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         no.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         tariff.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-
+        kalandk =(ProgressBar) findViewById(R.id.kalandk);
+        kalansms=(ProgressBar) findViewById(R.id.kalansms);
+        kalangb=(ProgressBar) findViewById(R.id.kalanint);
+        sumint = (TextView)findViewById(R.id.toplamint);
+        summin = (TextView)findViewById(R.id.toplamdk);
+        sumsms = (TextView)findViewById(R.id.toplamsms);
 
 
 
