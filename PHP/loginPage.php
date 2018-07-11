f519cf4d112fb3346aba45957b44666b669afc95
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2018-07-11 18:00:09 +0300

    Size Revize

--- PHP/loginPage.php
+++ PHP/loginPage.php
@@ -21,7 +21,7 @@ if($_SESSION){
 	<div class="row">
 		<div class="col col-10 col-mr-1 col-ml-1">
 			<div class="row">
-				<a href="http://i2i-systems.com/" class="col col-sm-12 a-center"><img src="img/i2iLogo.png"/></a>
+				<a href="http://i2i-systems.com/" class="col col-sm-12 a-center"><img src="img/i2iLogo.png" class="LoginLogo"/></a>
 			</div>
 		</div>
 	</div>
@@ -34,27 +34,27 @@ if($_SESSION){
 			<div class="box col-6 col-sm-12">
 				<div class="row">
 					<div class="box col-12 a-center">
-						<img src="img/cell2i-original.png" />
+						<img src="img/cell2i-original.png" class="LoginLogo2"/>
 					</div>
 				</div>
 				<div class="row">
 					<div class="box col-12">
 						
-							<input type="text" name="MSISDN" placeholder="Enter your phone number" class="box col-12 box-shadow1 border1 Segoe" maxLength="10"/>
+							<input type="text" name="MSISDN" placeholder="Enter your phone number" class="box col-sm-12 box-shadow1 border1 Segoe LoginInput" maxLength="10"/>
 						
 					</div>
 				</div>
 				<div class="row">
 					<div class="box col-12">
 						
-							<input type="password" name="Password" placeholder="Enter your password" class="box col-12 box-shadow1 border1 Segoe"/>
+							<input type="password" name="Password" placeholder="Enter your password" class="box col-sm-12 box-shadow1 border1 Segoe LoginInput"/>
 						
 					</div>
 				</div>
 				<div class="row">
 					<div class="col col-12">
 						
-							<a href="changePassword.php" class="col col-sm-12 a-right fr line-under color4 Segoe">Forgot your password?</a>
+							<a href="changePassword.php" class="col col-sm-12 a-right fr line-under color4 Segoe fsize20 fw700">Forgot your password?</a>
 						
 					</div>
 				</div>
@@ -73,7 +73,7 @@ if($_SESSION){
 					<div class="box col-12">
 						<div class="row">
 							<div class="box col-4 col-sm-1"></div>
-							<input type="submit" value="LOGIN" class="box col-4 col-sm-10 box-shadow1 border1 cPointer bg4 color1 fsize16 Segoe"/>
+							<input type="submit" value="LOGIN" class="box col-sm-10 box-shadow1 border1 cPointer bg4 color1 fsize20 Segoe LoginBtn"/>
 							<div class="box col-4 col-sm-1"></div>
 						</div>
 					</div>
