f4c223e2d6fa0ac045cfc4fc9d99841be79f4e8c
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2018-07-12 08:47:27 +0300

    LoginPage Design Revize

--- PHP/loginPage.php
+++ PHP/loginPage.php
@@ -38,28 +38,28 @@ if($_SESSION){
 					</div>
 				</div>
 				<div class="row">
-					<div class="box col-12">
+					<div class="box col-11">
 						
 							<input type="text" name="MSISDN" placeholder="Enter your phone number" class="box col-sm-12 box-shadow1 border1 Segoe LoginInput" maxLength="10"/>
 						
 					</div>
 				</div>
 				<div class="row">
-					<div class="box col-12">
+					<div class="box col-11">
 						
 							<input type="password" name="Password" placeholder="Enter your password" class="box col-sm-12 box-shadow1 border1 Segoe LoginInput"/>
 						
 					</div>
 				</div>
 				<div class="row">
-					<div class="col col-12">
+					<div class="box col-11 p-top">
 						
 							<a href="changePassword.php" class="col col-sm-12 a-right fr line-under color4 Segoe fsize20 fw700">Forgot your password?</a>
 						
 					</div>
 				</div>
 				<div class="row">
-					<div class="col col-12 color5 a-center Segoe">
+					<div class="box col-11 color5 a-center Segoe fsize24 p-left p-right fw300 lineHeight30">
 						<?php 
 							if($_POST){
 								$MSISDN = $_POST['MSISDN'];
@@ -70,7 +70,7 @@ if($_SESSION){
 					</div>
 				</div>
 				<div class="row">
-					<div class="box col-12">
+					<div class="box col-11">
 						<div class="row">
 							<div class="box col-4 col-sm-1"></div>
 							<input type="submit" value="LOGIN" class="box col-sm-10 box-shadow1 border1 cPointer bg4 color1 fsize20 Segoe LoginBtn"/>
