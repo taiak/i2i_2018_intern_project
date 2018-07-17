710ee5bb307e2d11c092a458bbebf7c0caf00285
Hakan <ipekhakancan@gmail.com>
2018-07-17 15:21:12 +0300

    7.17.18 Old and New Java Project Upd8

---
+++ Java Spring/Old Spring Web Project/src/main/java/com/i2i/cell/controller/DefaultController.java
@@ -0,0 +1,49 @@
+package com.i2i.cell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+	@GetMapping("/")
+	public String home1() {
+		return "/login";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
+
+	@GetMapping("/changePassword")
+	public String changePassword() {
+		return "/changePassword";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "/home";
+	}
+
+	@GetMapping("/message")
+	public String message() {
+		return "/message";
+	}
+
+	@GetMapping("/gifts")
+	public String gifts() {
+		return "/gifts";
+	}
+	
+	@GetMapping("/invoice")
+	public String invoive() {
+		return "/invoice";
+	}
+	
+	@GetMapping("/403")
+	public String error403() {
+		return "/error/403";
+	}
+
+}
