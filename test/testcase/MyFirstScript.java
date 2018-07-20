a3259717ffdb878e3fa4cd7ee95eecad52812221
tugba <tugbashn95@hotmail.com>
2018-07-20 10:24:53 +0300

    testcase
    testcase update

--- test/testcase/MyFirstScript.java
+++ test/testcase/MyFirstScript.java
@@ -1,3 +1,15 @@
+/*
+  author: Tugba Sahin
+  date: 14.07.2018
+  definition: Selenium WebDriver Automation
+  prerequisites:
+  Download and install JDK(Java SE Development Kit)
+  Download Eclipse IDE
+  Download Selenium WebDriver(selenium-server-standolone jar file) and add it to the library in your project in Eclipse IDE.
+  Download SafariDriver extension add that extension to your Safari browser.
+   
+*/
+
 package com.demo.testcases;
 
 import org.openqa.selenium.By;
@@ -11,7 +23,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("MSISDN")).sendKeys("5497952991");
 		driver.findElement(By.name("Password")).sendKeys("yanlisparola");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(19000);
+		Thread.sleep(2000);
 		if(driver.getPageSource().contains("Invalid Login"))
 		{ System.out.println("Please check your phone number and password then try again");}
 		else
@@ -23,7 +35,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("MSISDN")).sendKeys("");
 		driver.findElement(By.name("Password")).sendKeys("");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(19000);
+		Thread.sleep(2000);
 		if(driver.getPageSource().contains("Fields can not be blank"))
 		{ System.out.println("Fields can not be blank"); }
 		else
@@ -37,7 +49,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("Password")).sendKeys("turu");
 		driver.findElement(By.name("confirmPassword")).sendKeys("turuturu");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(19000);
+		Thread.sleep(2000);
 		if(driver.getPageSource().contains("Your passwords should match"))
 		{ System.out.println("Your passwords should match"); }
 		else
@@ -49,7 +61,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("Password")).sendKeys("turuturu");
 		driver.findElement(By.name("confirmPassword")).sendKeys("turuturu");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(11000);
+		Thread.sleep(2000);
 		if(driver.findElements(By.xpath("//body[contains(text(), 'Your password should be 8 character long and should contain at least one special character and number')]")) != null)
 		{ System.out.println("Your password should be 8 character long and should contain at least one special character and number"); }
 		else
@@ -62,7 +74,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("Password")).sendKeys("turuturu1.");
 		driver.findElement(By.name("confirmPassword")).sendKeys("turuturu1.");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(19000);
+		Thread.sleep(2000);
 		if(driver.getPageSource().contains("Password Confirmed"))
 		{ System.out.println("Password Confirmed"); }
 		else
@@ -74,7 +86,7 @@ public class MyFirstScript {
 		driver.findElement(By.name("MSISDN")).sendKeys("5497952991");
 		driver.findElement(By.name("Password")).sendKeys("turuturu1.");
 		driver.findElement(By.className("LoginBtn")).click();
-		Thread.sleep(29000);
+		Thread.sleep(9000);
 		if(driver.getPageSource().contains("Esnaf Tarifesi"))
 		{ System.out.println("Info Login"); }
 		else
@@ -83,7 +95,7 @@ public class MyFirstScript {
 	}
 	public static boolean CheckInvoice(WebDriver driver) throws InterruptedException{
 		driver.findElement(By.xpath("//*[@title='Faturalarım']")).click();
-		Thread.sleep(23000);
+		Thread.sleep(4000);
      	if(driver.getPageSource().contains("JULY INVOICE"))
 		{ System.out.println("Invoice Login"); }
      	else
@@ -92,7 +104,7 @@ public class MyFirstScript {
 	}
 	public static boolean CheckLogout(WebDriver driver) throws InterruptedException{
 		driver.findElement(By.className("invoiceLogo")).click();
-		Thread.sleep(25000);
+		Thread.sleep(6000);
 		driver.findElement(By.xpath("//*[@title='Oturumu Kapat']")).click();
 		if (driver.findElements(By.xpath("//body[contains(@class, 'LoginBtn')]")) != null)
 		{ System.out.println("Logout"); }
@@ -102,6 +114,8 @@ public class MyFirstScript {
 	}
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new SafariDriver();
+		//Resize Browser Window in WebDriver
+		driver.manage().window().maximize();
 
 		InvalidLogin(driver);
 		
